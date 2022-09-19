1c64f6fd8c2e68d15a7a9f85a0080699b9215ae9
m-oxu <dairenkonmajime@gmail.com>
2022-09-19 14:00:21 -0300

    Fixing error unt. 19 and using english names

--- first-list/ex007.c
+++ first-list/ex007.c
@@ -6,7 +6,7 @@
 /*   By: moxu <dairenkonmajime@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 01:23:38 by moxu              #+#    #+#             */
-/*   Updated: 2022/09/19 03:51:44 by moxu             ###   ########.fr       */
+/*   Updated: 2022/09/19 13:59:24 by moxu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	main(void)
 
 	printf("Input an integer number between 0 and 99: ");
 	scanf("%d", &number);
-	if (number <= 10)
+	if (number <= 19)
 	{
 		number_in_full_below_10(number);
 	}
@@ -50,19 +50,22 @@ int	main(void)
 void	number_in_full_below_10(int integer_number)
 {
 	int		i;
-	int		number_array_unt_10 [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	const char	*number_full_unt_10[] = {"Um", "Dois", "Três",
-		"Quatro", "Cinco",
-		"Seis", "Sete", "Oito", "Nove", "Dez"};
+	int		number_array_unt_19 [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+									11, 12, 13, 14, 15, 16, 17, 18, 19};
+	const char	*number_full_unt_19[] = {"One", "Two", "Three",
+										"Four", "Five", "Six",
+										"Seven", "Eight", "Nine", "Ten",
+										"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+										"Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
-	if (integer_number <= 10)
+	if (integer_number <= 19)
 	{
 		i = 0;
-		while (i < 10)
+		while (i < 19)
 		{
-			if (number_array_unt_10[i] == integer_number)
+			if (number_array_unt_19[i] == integer_number)
 			{
-				printf("Your number is %s. \n", number_full_unt_10[i]);
+				printf("Your number is %s. \n", number_full_unt_19[i]);
 			}
 			i++;
 		}
@@ -74,13 +77,15 @@ void	number_in_full_above_10(int integer_number, int division_rest_number)
 	int		i;
 	int		z;
 	int		number_array_10_in_10 [] = {20, 30, 40, 50, 60, 70, 80, 90};
-	int		number_array_unt_10 [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	const char	*number_full_10_in_10 [] = {"Vinte", "Trinta", "Quarenta",
-											"Cinquenta", "Sessenta", "Setenta",
-											"Oitenta", "Noventa"};
-	const char	*number_full_unt_10[] = {"Um", "Dois", "Três",
-										"Quatro", "Cinco", "Seis",
-										"Sete", "Oito", "Nove", "Dez"};
+	int		number_array_unt_19 [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+									11, 12, 13, 14, 15, 16, 17, 18, 19};
+	const char	*number_full_10_in_10 [] = {"Twenty", "Thirty", "Forty", "Fifty",
+											"Sixty", "Seventy", "Eighty", "Ninety"};
+	const char	*number_full_unt_19[] = {"One", "Two", "Three",
+										"Four", "Five", "Six",
+										"Seven", "Eight", "Nine", "Ten",
+										"Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+										"Seventeen", "Eighteen", "Nineteen"};
 
 		i = 0;
 	while (i < 8)
@@ -90,12 +95,12 @@ void	number_in_full_above_10(int integer_number, int division_rest_number)
 			if (division_rest_number > 0)
 			{
 				z = 0;
-				while (z < 10)
+				while (z < 19)
 				{
-					if (number_array_unt_10[z] == division_rest_number)
+					if (number_array_unt_19[z] == division_rest_number)
 					{
-						printf("Your number is %s e %s. \n",
-							number_full_10_in_10[i], number_full_unt_10[z]);
+						printf("Your number is %s %s. \n",
+							number_full_10_in_10[i], number_full_unt_19[z]);
 					}
 					z++;
 				}
